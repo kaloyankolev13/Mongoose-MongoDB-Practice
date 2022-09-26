01381eeda27a25141cc8c814fd82159887c8b6cc
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-09-27 00:27:31 +0200

    created a model

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
 async function main() {
-  mongoose.connect('mongodb://localhost:27017/MovieApp');
+  mongoose.connect('mongodb://localhost:27017/movieApp');
   console.log('Connected to MongoDB');
 }
 
 main().catch((err) => console.log(err));
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  score: Number,
+  rating: String,
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+const amadeus = new Movie({
+  title: 'Amadeus',
+  year: 1986,
+  score: 9.2,
+  rating: 'R',
+});

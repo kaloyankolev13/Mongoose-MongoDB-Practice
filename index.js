561a10c7ea3c61a04f0c6bd47ac59901a7228f99
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-09-27 18:47:13 +0200

    added static data abd created a Schema

--- index.js
+++ index.js
@@ -21,3 +21,39 @@ const amadeus = new Movie({
   score: 9.2,
   rating: 'R',
 });
+
+Movie.insertMany([
+  {
+    title: 'Amelie',
+    year: 2001,
+    score: 8.3,
+    rating: 'R',
+  },
+  {
+    title: 'Alien',
+    year: 1979,
+    score: 8.1,
+    rating: 'R',
+  },
+  {
+    title: 'the Iron Giant',
+    year: 1999,
+    score: 7.5,
+    rating: 'PG',
+  },
+  {
+    title: 'Stand By Me',
+    year: 1986,
+    score: 8.6,
+    rating: 'R',
+  },
+  {
+    title: 'Moonrise Kingdom',
+    year: 2012,
+    score: 7.3,
+    rating: 'PG-13',
+  },
+]).then((data) => {
+  console.log('It worked!');
+  console.log(data);
+});

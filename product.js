794c3b02590085a5270c86e3320cc66881807497
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-09-27 20:02:54 +0200

    looked into Mongoose schema validations and constraints

--- product.js
+++ product.js
@@ -11,18 +11,35 @@ const productSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    maxLength: 20,
   },
   price: {
     type: Number,
     required: true,
+    min: 0,
+  },
+  onSale: {
+    type: Boolean,
+    default: false,
+  },
+  categories: [String],
+  qty: {
+    online: {
+      type: Number,
+      default: 0,
+    },
+    inStore: {
+      type: Number,
+      default: 0,
+    },
   },
 });
 
 const Product = mongoose.model('Product', productSchema);
 const bike = new Product({
-  name: 'Mountain Bike',
-  price: 999,
-  color: 'red',
+  name: 'Bike Helmet',
+  price: 19.5,
+  categories: ['Cycling', 'Safety'],
 });
 bike
   .save()
